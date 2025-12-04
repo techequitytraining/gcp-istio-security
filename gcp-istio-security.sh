9bb751bdcfa606ddb0c2460a178ae1f82107ecc9
shiyghannavti <136981389+shiyghannavti@users.noreply.github.com>
2025-12-04 18:39:54 +0000

    Add developer contact info to welcome message
    Added developer contact information to welcome message.

--- gcp-istio-security.sh
+++ gcp-istio-security.sh
@@ -48,6 +48,7 @@ echo
 echo -e "                        👋  Welcome to Cloud Sandbox! 💻"
 echo 
 echo -e " Visit https://docs.techequity.cloud for terraform based rapid application deployment"
+echo -e "          Developed by: Shiyghan Navti shiyghan.navti@techequity.cloud"
 echo 
 echo -e "              *** PLEASE WAIT WHILE LAB UTILITIES ARE INSTALLED ***"
 sudo apt-get -qq install pv > /dev/null 2>&1
