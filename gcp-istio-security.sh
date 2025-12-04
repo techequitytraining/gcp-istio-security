86c96fc353b77a1169634eed7ff0bced1b0f8760
shiyghannavti <136981389+shiyghannavti@users.noreply.github.com>
2025-12-04 18:13:03 +0000

    Update gcp-istio-security.sh

--- gcp-istio-security.sh
+++ gcp-istio-security.sh
@@ -47,6 +47,8 @@ echo
 echo
 echo -e "                        👋  Welcome to Cloud Sandbox! 💻"
 echo 
+echo -e " Visit https://docs.techequity.cloud for terraform based rapid application deployment"
+echo 
 echo -e "              *** PLEASE WAIT WHILE LAB UTILITIES ARE INSTALLED ***"
 sudo apt-get -qq install pv > /dev/null 2>&1
 echo 
